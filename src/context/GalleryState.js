f8db0829bfaba9eef9bb62517b85337233535b85
kaku-kuo <z22857744@gmail.com>
2020-04-08 00:07:57 +0800

    ready to depoly

--- src/context/GalleryState.js
+++ src/context/GalleryState.js
@@ -5,7 +5,13 @@ import GalleryReducer from './galleryReducer';
 import {SEARCH_PHOTOS,SEARCH_USER,GET_USER_PHOTOS,SET_LOADING,SET_LOCATION} from './type'
 
 
-let key = 'tEscdJTacKLUdc1YMTWTkWHcKAtgIchVhoi4swbHjw8';
+let galleryClientId;
+
+if(process.env.NODE_ENV !== "production"){
+    galleryClientId = process.env.REACT_APP_GALLERY_CLIENT_ID;
+}else{
+    galleryClientId = process.env.GALLERY_CLIENT_ID;
+}
 
 
 const GalleryState = props => {
@@ -23,19 +29,19 @@ const GalleryState = props => {
     // Search photos
     const searchPhotos = async text => {
         setLoading();
-        const res = await axios.get(`https://api.unsplash.com/photos/random/?client_id=${key}&count=10&query=${text}`);
+        const res = await axios.get(`https://api.unsplash.com/photos/random/?client_id=${galleryClientId}&count=10&query=${text}`);
         dispatch({type:SEARCH_PHOTOS,payload:res.data});
     }
     // Search User
     const searchUser = async username => {
         setLoading();
-        const res = await axios.get(`https://api.unsplash.com/users/${username}/?client_id=${key}`);
+        const res = await axios.get(`https://api.unsplash.com/users/${username}/?client_id=${galleryClientId}`);
         dispatch({type:SEARCH_USER,payload:res.data});
     }
     // Get user photos
     const getUserPhotos = async username => {
         setLoading();
-        const res = await axios.get(`https://api.unsplash.com/users/${username}/photos/?client_id=${key}&per_page=30`);
+        const res = await axios.get(`https://api.unsplash.com/users/${username}/photos/?client_id=${galleryClientId}&per_page=30`);
         dispatch({type:GET_USER_PHOTOS,payload:res.data});
        
     }
